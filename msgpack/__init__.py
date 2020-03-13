9608fff6a8d3d8bd2bdb10bee8589c492db4f3aa
Krzysztof Klinikowski <kkszysiu@gmail.com>
2020-03-13 08:57:43 +0700

    Force use C version

--- msgpack/__init__.py
+++ msgpack/__init__.py
@@ -8,14 +8,6 @@ import sys
 
 from ._cmsgpack import Packer, unpackb, Unpacker
 
-if os.environ.get("MSGPACK_PUREPYTHON"):
-    from .fallback import Packer, unpackb, Unpacker
-else:
-    try:
-        from ._cmsgpack import Packer, unpackb, Unpacker
-    except ImportError:
-        from .fallback import Packer, unpackb, Unpacker
-
 def pack(o, stream, **kwargs):
     """
     Pack object `o` and write it to `stream`
